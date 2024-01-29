91e81811689159fe1535860f35a2c93cf718986d
Kuba Świerlikowski <j.swierlik@o2.pl>
2024-01-29 19:02:58 +0100

    pooprawki 3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     jobs = load_jobs_from_db()
     return render_template(
         'home.html',
@@ -14,36 +14,48 @@ def hello_world():
         jobs=jobs,
     )
 
-@app.route('/api/jobs')
-def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
+@app.route('/posluchaj')
+def posluchaj():
+    return render_template('listen.html')
 
-@app.route('/jobs/<id>')
-def show_job(id):
-    job = load_specific_job_from_db(id)
-    if not job:
-        return "Not Found", 404
-    else:
-        return render_template(
-            'jobpage.html',
-            job=job
-        )
+@app.route('/oferta')
+def oferta():
+    return render_template('oferta.html')
 
-@app.route("/jobs/<id>/apply", methods=['post'])
-def apply_to_job(id):
-    data = request.form
-    job = load_specific_job_from_db(id)
-    add_application_to_db(id, data)
-    return render_template(
-        'application_submitted.html',
-        application=data
-    )
+@app.route('/cennik')
+def cennik():
+    return render_template('prices.html')
 
-@app.route('/api/jobs/<id>')
-def api_show_job(id):
-    job = load_specific_job_from_db(id)
-    return jsonify(job)
+# @app.route('/api/jobs')
+# def list_jobs():
+#     jobs = load_jobs_from_db()
+#     return jsonify(jobs)
+
+# @app.route('/jobs/<id>')
+# def show_job(id):
+#     job = load_specific_job_from_db(id)
+#     if not job:
+#         return "Not Found", 404
+#     else:
+#         return render_template(
+#             'jobpage.html',
+#             job=job
+#         )
+
+# @app.route("/jobs/<id>/apply", methods=['post'])
+# def apply_to_job(id):
+#     data = request.form
+#     job = load_specific_job_from_db(id)
+#     add_application_to_db(id, data)
+#     return render_template(
+#         'application_submitted.html',
+#         application=data
+#     )
+
+# @app.route('/api/jobs/<id>')
+# def api_show_job(id):
+#     job = load_specific_job_from_db(id)
+#     return jsonify(job)
 
 
 if __name__ == '__main__':
